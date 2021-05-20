dd8f2d1d3ad9360e3ff9f92ec72ad32bebbbc2b2
sumedha60 <76279963+sumedha60@users.noreply.github.com>
2021-05-20 14:23:04 +0530

    Add files via upload

---
+++ package.js
@@ -0,0 +1,16 @@
+class package{
+    constructor (){
+    Matter .body.setStatic(packageBody,isStatic)
+      }
+
+      display(){
+        push ()
+        rectMode(CENTER)
+        translate (this.body.position.x,this.body.position.y)
+        rotate (packageBody.body.angle)
+        rect(0,0,this.width,this.height)
+      
+        pop ()
+      }
+
+}
